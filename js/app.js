2b4d3af6141f3dcf7ff0504ad66c30cd90b32488
Sebastiao de Paula <sebastiao.paula@gmail.com>
2025-01-24 23:38:15 -0300

    Ajustada a function sortear

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
     
-    amigos.push(amigos.values);
+    amigos.push(amigo.value);
 
     if (lista.textContent == ''){
         lista.textContent = amigo.value;
@@ -15,8 +15,21 @@ function adicionar() {
 }
 
 function sortear(){
-    embaralha(amigos)
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    sorteio.innerHTML = '';
 
+    for (let i = 0; i < amigos.length; i++){
+        if (i == amigos.length -1){ 
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+        }else{
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+        }
+        // uma forma de fazer
+        /*let proximo = (i + 1) % amigos.length;       
+        sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[proximo] + '<br>';
+        */
+    }
 }
 
 function embaralha(lista){
